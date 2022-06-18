d26984a48c9fb3a87cf8b7652c895df90591f8be
Bobby Lee <bobby@bobbylee.dev>
2022-06-18 14:34:51 -0700

    update: header logo to svg

--- wp-content/themes/sapphire/blocks/header/header.php
+++ wp-content/themes/sapphire/blocks/header/header.php
@@ -3,8 +3,8 @@
 	<div class="header-social"><?php echo social_media(); ?></div>
 
 	<div class="site-branding">
-		<!-- <div class="header-logo"><?php// echo inline_svg('chef-corgi1') ?></div> -->
-		<div class="header-logo"><img src="<?php bloginfo('template_url'); ?>/assets/svg/chef-corgi.svg"/></div>
+		<div class="header-logo"><?php echo inline_svg('chef-corgi1') ?></div>
+		<!-- <div class="header-logo"><img src="<?php// bloginfo('template_url'); ?>/assets/svg/chef-corgi.svg"/></div> -->
 
 		<?php echo site_title(); ?>
 	</div>
